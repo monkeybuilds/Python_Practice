ec756498a2bf41a8c8851bd5e1b4978431e8a9dc
monkeybuilds <atharvapathak025@gmail.com>
2025-06-07 15:57:34 +0530

    Add files via upload

---
+++ love calculator.py
@@ -0,0 +1,16 @@
+
+correction = []
+def calculate_love_score(name1, name2):
+    for letter in name1:
+        letter = correction
+        print(letter)
+    for letter in name2:
+        letter = correction
+        print(letter)
+
+a = correction.count(letter)
+print (a)
+
+
+
+calculate_love_score(name1="alice" , name2="boobs")
