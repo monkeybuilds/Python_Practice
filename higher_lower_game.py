6747ab825b59cb0db944c1278e2d4fa9cc84eb65
monkeybuilds <atharvapathak025@gmail.com>
2025-06-13 23:46:50 +0530

    Add files via upload

---
+++ higher_lower_game.py
@@ -0,0 +1,7 @@
+print("Welcome to Higher_Lower_Game")
+print("In this you will be provided to guess the age of celebrities")
+bollywood_actors = ["Shah Rukh Khan", "Ranbir Kapoor", "Amitabh Bachchan", "Salman Khan", "Hrithik Roshan"]
+indian_singers = ["Arijit Singh", "Shreya Ghoshal", "Neha Kakkar", "KK", "Sidhu Moosewala"]
+
+
+
